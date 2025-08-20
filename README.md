66e8a75d60c4b837b57e5ee97ddd5a2d91d59b26
melabreu <melissa.abreu84@gmail.com>
2025-08-19 23:19:42 -0400

    Updated Project

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-# concentration-game
+# Concentration Game
+![Concentration Game](https://i.imgur.com/H7I8Hjw.png)
+
+## What is the Concentration Game?
+The concentration Game is a memory game where each block/button will have an image. The goal of the game is to match all 3 pair of images at a time.
+
+## Why this game?
+Other than it being a fun and challanging game, the layout of the concentration game is similar to a previoud tic-tac-toe lab. I felt more comfortable applying this concept, to focus on learning additional rules and functions.
+
+## Attributions
+While working on this project, one of my challenges was trying to determine which attributes, properties and elements to use. Some of the things I learned and implimented were:
++ The `<dialog>` HTML element, which tells the browser that its contents represents a dialog.
+    + It was used for an end-of-game dialog. Ex. the end-of-game dialog was closed using `endDialogEl.open` -> `endDialogEl.close`.
+    + The modal support `showModal()`, which captures focus and keeps the user from interacting with the rest of the page.
+
+    For more information on `<dialog>` and `showModal()`, visit [MDN: The Dialog Element](https://developer.mozilla.org/en-US/docs/Web/HTML/Reference/Elements/dialog).
+
++ The `grid-template` property, which helped me define the structure of grid of the columns and rows. I used `repeat(3, 110px)` to define columns/rows amount(3) and its size(110px).
+    + Why I chose this? I built a grid container using `display: grid;`, and then defined it using rows and columns.
+
+    For more information on `grid-template`, visit [MDN: grid-template](https://developer.mozilla.org/en-US/docs/Web/CSS/grid-template).
+
++ The `innerHTML` property was used to clear the board, which was something I had not used before. This, gets or sets the HTML markup in the element.
+
+    For more information on `innerHTML`, visit [MDN: Element: innerHTML property](https://developer.mozilla.org/en-US/docs/Web/API/Element/innerHTML).
+
++ The **Fisher-Yates (Durstenfeld) shuffle** alforithm, Which swaps each element with another randomly selected one, without it being bias. The code was the following:
+    ```
+    for(let i = deck.length - 1; i > 0; i--) {
+        const a = Math.floor(Math.random() * (i + 1));
+        [deck[i], deck[a]] = [deck[a], deck[i]];
+    }
+    ```
+    For more information on **Fisher-Yates (burstenfiel) shuffle**, visit [Wikipedia: Fisher-Yates Shuffle](https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle).
+
++ The event: `currentTarget` property, which is the element to which the event handler is attached to.
+    + Using it in `onCardClick` registers the click handler for the card button.
+
+    For more information on `currentTarget`, visit [MDN: Event: currentTarget property](https://developer.mozilla.org/en-US/docs/Web/API/Event/currentTarget).
+
++ The `element.dataset` propety, which gives access to data attributes on en element.
+    + Using it in `onCardClick` reads the card's index from a `data-index` attribute.
+
+    For more information on `element.dataset`, visit [MDN: HTMLElement: dataset property](https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/dataset).
+
++ The `isSet()` function helped me check if the currently selected cards all have the same set, inside the array `every()`.
+
+    For more information about Array.prototype.every(), visit [MDN: Array.prototype.evey()](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/every)
+
+## Technologies Used
++ HTML
++ JavaScript
++ CSS
+
+## Next Steps
+I plan to keep practicing browser-based-games, because it was fun and challenging. I want to be able to improve in this area and be able to do more challengin games.
---
+++ js/app.js
@@ -0,0 +1,148 @@
+/*-------------------------------- Constants --------------------------------*/
+
+const rows = 3, cols = 3;
+const setSize = 3;
+const cardCount = rows * cols;
+const totalSets = cardCount / setSize;
+
+/*---------------------------- Variables (state) ----------------------------*/
+
+let deck = [];
+let selection = [];
+let lock = false;
+let moves = 0, matches = 0;
+let won;
+
+/*------------------------ Cached Element References ------------------------*/
+
+const boardEl = document.querySelector('.board');
+const restartBtnEl = document.getElementById('restart');
+const matchesEls = document.getElementById('matches');
+const movesEls = document.getElementById('moves');
+const endDialogEl = document.getElementById('endDialog');
+const endMessageEl = document.getElementById('endMessage');
+
+console.log(matchesEls);
+console.log(movesEls);
+
+/*-------------------------------- Functions --------------------------------*/
+
+initGame();
+
+function initGame() {
+    deck = [];
+    for(let v = 1; v <= totalSets; v++) {
+        for(let i = 0; i < setSize; i++)
+            deck.push({ value: v, isUp: false, isMatched: false, el: null
+        });
+    }
+    for(let i = deck.length - 1; i > 0; i--) {
+        const a = Math.floor(Math.random() * (i + 1));
+        [deck[i], deck[a]] = [deck[a], deck[i]];
+    }
+    console.log("Deck:", deck);
+
+    selection = [];
+    lock = false;
+    moves = 0;
+    matches = 0;
+
+    boardEl.innerHTML = '';
+    deck.forEach((card, i) => {
+        const btn = document.createElement('button');
+        btn.className = 'card';
+        btn.type = 'button';
+        btn.dataset.index = String(i);
+        btn.ariaLabel = 'Card';
+        btn.addEventListener('click', onCardClick);
+        boardEl.appendChild(btn);
+
+        card.el = btn;
+        renderCard(i); //face down
+    });
+    updateBoard();
+    if (endDialogEl?.open) endDialogEl.close();
+}
+
+function onCardClick(e) {
+    if (lock) return;
+
+    const el = e.currentTarget;
+    const index = Number(el.dataset.index);
+    const card = deck[index];
+
+    console.log("Card clicked:", index, card);
+
+    if (card.isMatched || card.isUp) return;
+
+    card.isUp = true;   //face up
+    selection.push(index);
+    renderCard(index);
+
+    console.log("Current selection:", selection.map(i => deck[i].value));
+
+    if(selection.length < setSize) return;
+
+    moves++
+    updateBoard();
+
+    if (isSet()) {
+        const setValues = selection.map(i => deck[i].value);
+        selection.forEach(i => {
+            deck[i].isMatched = true;
+            renderCard(i);
+        });
+        selection = [];
+        matches++;
+        updateBoard();
+
+        if (matches === totalSets) {
+            if (endMessageEl) endMessageEl.textContent = `You win in ${moves} moves!`;
+            if (endDialogEl && !endDialogEl.open) endDialogEl.showModal();
+        }
+        console.log("Set found:", setValues);
+    } else {
+        lock = true;
+        setTimeout(() => {
+            selection.forEach(i => {
+                deck[i].isUp = false;
+                renderCard(i);
+            });
+            selection = [];
+            lock = false;
+    });
+    console.log("Game ended", won, "Moves:", moves, "Matches:", matches);
+    }
+}
+
+function isSet() {
+    const firstValue = deck[selection[0]].value;
+    return selection.every(i => deck[i].value === firstValue);
+}
+
+function renderCard(index) {
+    const card = deck[index];
+    const el = card.el;
+    if (!el) return;
+
+    el.classList.remove('v1', 'v2', 'v3', 'up', 'matched');
+
+    if (card.isUp || card.isMatched) {
+        el.classList.add('up', `v${card.value}`);
+    }
+    if (card.isMatched) {
+        el.classList.add('matched');
+        el.disabled = true;
+    } else {
+        el.disabled = false;
+    }
+}
+
+function updateBoard() {
+    movesEls.textContent = String(moves);
+    matchesEls.textContent = String(matches);
+}
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+restartBtnEl.addEventListener('click', initGame);
